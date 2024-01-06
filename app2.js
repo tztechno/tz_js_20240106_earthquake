523ca7baaa2542a9041d4b9b8d4275ac1b9929d4
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-06 19:09:20 +0900

    Update and rename xxx1 to app2.js

---
+++ app2.js
@@ -0,0 +1,78 @@
+const csvFilePath = 'ishikawa_202401.csv';
+
+
+fetch(csvFilePath)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Failed to fetch CSV, status ${response.status}`);
+        }
+        return response.text();
+    })
+    .then(csvData => {
+        const earthquakeData = parseCSV(csvData);
+        drawChart(earthquakeData);
+    })
+    .catch(error => console.error('Error:', error));
+
+
+function parseCSV(csv) {
+    const lines = csv.split('\n');
+    const headers = lines[0].split(',');
+    const data = [];
+    for (let i = 1; i < lines.length; i++) {
+        const values = lines[i].split(',');
+        const entry = {};
+        for (let j = 0; j < headers.length; j++) {
+            entry[headers[j]] = values[j];
+        }
+        data.push(entry);
+    }
+    return data;
+}
+
+
+function drawChart(earthquakeData) {
+    const latitude = earthquakeData.map(entry => entry.latitude);
+    const magnitudes = earthquakeData.map(entry => entry.mag);
+
+    const ctx = document.getElementById('earthquakeChart').getContext('2d');
+
+    const myChart = new Chart(ctx, {
+
+        type: 'scatter', // チャートのタイプを散布図に設定
+        data: {
+            datasets: [{
+                label: 'Earthquake Locations',
+                data: earthquakeData.map(entry => ({ x: entry.latitude, y: entry.mag })),
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.5)',
+            }]
+        },
+
+
+        options: {
+            scales: {
+                x: {
+                    type: 'linear',
+                    position: 'bottom',
+                    title: {
+                        display: true,
+                        text: 'Latitude',
+                    }
+                },
+                y: {
+                    type: 'linear',
+                    position: 'left',
+                    title: {
+                        display: true,
+                        text: 'Magnitude2',
+                    },
+                    ticks: {
+                    min: 4, // Y軸の最小値
+                    max: 8, // Y軸の最大値
+                    }
+                }
+            }
+        }
+    });
+}
