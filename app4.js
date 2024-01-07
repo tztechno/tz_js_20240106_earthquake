845402841caa26b69fa0c136a033131dc8b1e394
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-07 12:41:10 +0900

    Update app4.js

--- app4.js
+++ app4.js
@@ -27,13 +27,16 @@ function parseCSV(csv) {
         for (let j = 0; j < headers.length; j++) {
             const key = headers[j].trim();  // Remove leading/trailing whitespaces from headers
             let value = values[j].trim();  // Remove leading/trailing whitespaces from values
+
             // Convert 'time' column to Date object
             if (key === 'time') {
                 value = new Date(value);
-            } else if (key === 'mag') {
+            // } else if (key === 'mag') {
+            } else {
                 // Remove any non-numeric characters before parsing
                 value = parseFloat(value.replace(/[^\d.]/g, ''));
             }
+
             entry[key] = value;
         }
         data.push(entry);
