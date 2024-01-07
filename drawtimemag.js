22383fc72ca67a5bbd8189a742b08dd8d5c88868
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-08 01:42:27 +0900

    Update drawtimemag.js

--- drawtimemag.js
+++ drawtimemag.js
@@ -1,10 +1,7 @@
+
 function drawChart(earthquakeData) {
     const time = earthquakeData.map(entry => moment(entry.time));
     const magnitudes = earthquakeData.map(entry => entry.mag);
-
-    // Output data to console
-    console.log('Earthquake Data:', earthquakeData);
-
     const ctx = document.getElementById('earthquakeChart').getContext('2d');
 
     const myChart = new Chart(ctx, {
