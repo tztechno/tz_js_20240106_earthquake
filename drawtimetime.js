b581f25736ecfffa1bc39742ced972bf4578e71c
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-08 14:26:57 +0900

    Update drawtimetime.js

--- drawtimetime.js
+++ drawtimetime.js
@@ -1,3 +1,16 @@
+fetch(csvFilePath)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Failed to fetch CSV, status ${response.status}`);
+        }
+        return response.text();
+    })
+    .then(csvData => {
+        const earthquakeData = parseCSV(csvData);
+        drawChart(earthquakeData);
+    })
+    .catch(error => console.error('Error:', error));
+
 
 function drawChart(earthquakeData) {
     const time = earthquakeData.map(entry => entry.time);
