4fa5fee89b0ccdf1193417e7169bcea9bceab23c
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-08 14:31:15 +0900

    Update drawlatmag.js

--- drawlatmag.js
+++ drawlatmag.js
@@ -1,3 +1,17 @@
+fetch(csvFilePath)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Failed to fetch CSV, status ${response.status}`);
+        }
+        return response.text();
+    })
+    .then(csvData => {
+        const earthquakeData = parseCSV(csvData);
+        drawChart(earthquakeData);
+    })
+    .catch(error => console.error('Error:', error));
+
+
 function drawChart(earthquakeData) {
     const latitude = earthquakeData.map(entry => entry.latitude);
     const magnitudes = earthquakeData.map(entry => entry.mag);
