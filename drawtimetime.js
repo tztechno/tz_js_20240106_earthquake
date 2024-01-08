1570ed4889cdadc7a6bfc4371d880fe5c10a0276
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-08 15:41:40 +0900

    Update drawtimetime.js

--- drawtimetime.js
+++ drawtimetime.js
@@ -38,7 +38,7 @@ function drawChart(earthquakeData) {
                     }
                 },
                 y: {
-                    type: 'category',
+                    type: 'linear',
                     position: 'bottom',
                     title: {
                         display: true,
